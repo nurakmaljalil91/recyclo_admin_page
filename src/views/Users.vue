3f00dfab1c2468bf52ecda130e5210d9581f778b
nurakmaljalil91 <nurakmaljalil91@gmail.com>
2020-11-18 17:33:51 +0800

    add user new and edit

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -13,12 +13,8 @@
 
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn color="primary" text @click="dialog = false">
-              Yes
-            </v-btn>
-            <v-btn color="red" text @click="dialog = false">
-              No
-            </v-btn>
+            <v-btn color="primary" text @click="dialog = false"> Yes </v-btn>
+            <v-btn color="red" text @click="dialog = false"> No </v-btn>
           </v-card-actions>
         </v-card>
       </v-dialog>
@@ -29,9 +25,57 @@
         <v-card-title
           >Manage Users
           <v-spacer></v-spacer>
-          <v-btn color="primary" dark class="mb-2" v-bind="attrs" v-on="on">
-            New Item
-          </v-btn>
+          <v-dialog v-model="dialog" max-width="500px">
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn color="primary" dark class="mb-2" v-bind="attrs" v-on="on">
+                New User
+              </v-btn>
+            </template>
+            <v-card>
+              <v-card-title>
+                <span class="headline">{{ formTitle }}</span>
+              </v-card-title>
+
+              <v-card-text>
+                <v-container>
+                  <v-row>
+                    <v-col cols="12">
+                      <v-text-field
+                        v-model="editedUser.username"
+                        label="Username"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12">
+                      <v-text-field
+                        v-model="editedUser.password"
+                        label="Password"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12">
+                      <v-text-field
+                        v-model="editedUser.email"
+                        label="Email"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12">
+                      <v-text-field
+                        v-model="editedUser.points"
+                        label="Points"
+                      ></v-text-field>
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-card-text>
+
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="blue darken-1" text @click="close">
+                  Cancel
+                </v-btn>
+                <v-btn color="blue darken-1" text @click="save"> Save </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
         </v-card-title>
 
         <template>
@@ -78,6 +122,19 @@ export default {
       users: [],
       dialog: false,
       dialogDelete: false,
+      editedIndex: -1,
+      editedUser: {
+        username: "",
+        password: "",
+        email: "",
+        points: 0,
+      },
+      defaultUser: {
+        username: "",
+        password: "",
+        email: "",
+        points: 0,
+      },
     };
   },
   created() {
@@ -94,8 +151,31 @@ export default {
       });
     });
   },
+  computed: {
+    formTitle() {
+      return this.editedIndex === -1 ? "New User" : "Edit User";
+    },
+  },
+  watch: {
+    dialog(val) {
+      val || this.close();
+    },
+    dialogDelete(val) {
+      val || this.closeDelete();
+    },
+  },
   methods: {
-    editUser() {},
+    newUser() {},
+    editUser(docId) {
+      this.users.forEach(element => {
+        if(element.id == docId){
+          this.editedIndex = docId;
+          console.log(this.editedIndex);
+        }
+      });
+      this.editedUser = Object.assign({}, docId)
+      this.dialog = true
+    },
     deleteUser(id) {
       db.collection("users")
         .doc(id)
@@ -106,6 +186,22 @@ export default {
         .catch((error) => {
           console.log(error);
         });
+    },
+    close() {
+      this.dialog = false;
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+      });
+    },
+
+    save () {
+      if (this.editedIndex > -1) {
+        Object.assign(this.desserts[this.editedIndex], this.editedItem)
+      } else {
+        this.desserts.push(this.editedItem)
+      }
+      this.close()
     },
   },
 };
